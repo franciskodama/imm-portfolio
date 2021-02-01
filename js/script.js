11a48402bc09fc7f56845715538f0a9e545253b2
Kodama <fk@fkodama.com>
2021-01-31 22:04:14 -0300

    Include menu hamburguer

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-
+// Buttons | Smooth navigation + Unhide content behind (position - 70px) -----------
 
 const links = document.querySelectorAll(".js-button");
 
@@ -16,3 +16,27 @@ function clickHandler(e) {
     behavior: "smooth"
   });
 }
+
+
+
+// Mobile Hamburguer Menu | Open and Close ---------------------------------------
+
+const closeButton = document.querySelector('.close-nav');
+const openButton = document.querySelector('.open-nav');
+const nav = document.querySelector('nav');
+
+closeButton.addEventListener("click", () => {
+  nav.classList.remove('navigation-open');
+});
+
+openButton.addEventListener("click", () => {
+  nav.classList.add('navigation-open');
+});
+
+const closeButtonAfterClick = document.querySelectorAll('.js-button');
+for (var i = 0; i < closeButtonAfterClick.length; i++) {
+  closeButtonAfterClick[i].addEventListener("click", () => {
+    nav.classList.remove('navigation-open');
+  });
+}
+
